7804ff66e4dc19a7fe3f90bc4e9017f2bc5e477d
Ashadujjaman50 <ashadujjaman.cse@gmail.com>
2026-01-22 11:35:51 +0600

    release new version 1.0.2

--- loadingdialog/build.gradle.kts
+++ loadingdialog/build.gradle.kts
@@ -50,7 +50,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.Ashadujjaman50"
             artifactId = "loadingdialog"
-            version = "1.0.1"
+            version = "1.0.2"
 
             afterEvaluate {
                 from(components["release"])
